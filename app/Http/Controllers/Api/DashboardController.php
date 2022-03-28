f3406adeeb46c5c25fff4eba2ed49b96d4fa36b9
Rupom Ehsan <rupomehsan34@gmail.com>
2022-03-28 19:49:07 +0600

    Product & Category Api

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function index()
+    {
 
+    }
 }
